7a716da463d46ae1d24e5b299bca76b88a18000d
Astyr <astyr.github@gmail.com>
2020-07-30 22:29:03 +0200

    commit on 'master'

---
+++ practicepython.org/Exercise1.py
@@ -0,0 +1,4 @@
+name = str(input("What's your name ? -> "))
+age = int(input("How old are you ? -> "))
+
+print("Hi " + name + ", nice to meet you, you will get 100 years old in " + str(int(100) - age) + " years !")
