60f60522939218e5e6dd57e39376e29d864f93b6
Astyr <astyr.github@gmail.com>
2020-07-31 17:34:59 +0200

    commit on 'master'

---
+++ Higher or Lower/HoL.py
@@ -0,0 +1,96 @@
+from getpass import getpass
+import random
+import os
+clear = lambda: os.system('cls')
+clear()
+
+# ENDLESS LOOP
+
+while True:
+    print("==================================================")
+    print("Higher or Lower ?")
+    print("==================================================")
+    print("")
+    print("New game ?")
+    ng = input("(Y)es / (N)o > ")
+
+# GAME BLOCK
+
+    if ng == "Y" or ng == "y":
+
+        ng = None
+        clear()
+        print("==================================================")
+        print("Higher or Lower ?")
+        print("==================================================")   
+        print("")
+        print("Difficulty Level :")
+        print("(1) Baby")
+        print("(2) Easy")
+        print("(3) Medium")
+        print("(4) Hard")
+        print("(5) Demon")
+        print("(6) Is this even possible ?")
+        lvl = input("> ")
+
+        tries = 1
+        num = 0
+
+        clear()
+        print("==================================================")
+        print("Higher or Lower ?")
+        print("==================================================")
+
+        if lvl == "1": num = random.randint(1, 11); print("Between 1 and 10")
+
+        elif lvl == "2": num = random.randint(1, 21); print("Between 1 and 20")
+
+        elif lvl == "3": num = random.randint(1, 51); print("Between 1 and 50")
+
+        elif lvl == "4": num = random.randint(1, 101); print("Between 1 and 100")
+
+        elif lvl == "5": num = random.randint(1, 1001); print("Between 1 and 1000")
+
+        elif lvl == "6": num = random.randint(1, 1000000001); print("Between 1 and 1000000000")
+
+        else: clear(); continue
+
+        print("")
+        
+        while True:
+            try:
+                ipt = int(input("Type you guess > "))
+                if ipt == num:
+                    break
+                else:
+                    tries += 1
+                    clear()
+                    print("==================================================")
+                    print("Higher or Lower ?")
+                    print("==================================================")
+                    print("")
+                    if ipt < num:
+                        print("Higher !")
+                    elif ipt > num:
+                        print("Lower !")
+            except ValueError:
+                pass
+                
+                
+
+        print("")
+        print("------------------------------------------")
+        print("You guessed tne number " + str(num) + " in " + str(tries) + " try/ies !")
+        print("------------------------------------------")
+        print("")
+        getpass("Press Enter to continue...")
+        clear()
+
+# EXIT BLOCK
+
+    elif ng == "N" or ng == "n":
+        ng = None
+        break
+
+    else:
+        clear()
--- practicepython.org/Exercise1.py
+++ practicepython.org/Exercise1.py
@@ -1,4 +1,6 @@
-name = str(input("What's your name ? -> "))
-age = int(input("How old are you ? -> "))
+# name = str(input("What's your name ? -> "))
+# age = int(input("How old are you ? -> "))
 
-print("Hi " + name + ", nice to meet you, you will get 100 years old in " + str(int(100) - age) + " years !")
+# print("Hi " + name + ", nice to meet you, you will get 100 years old in " + str(int(100) - age) + " years !")
+
+print("Hi " + str(input("What's your name ? > ")) + ", nice to meet you, you will get 100 years old in " + str(int(100) - int(input("How old are you ? > "))) + " years !")
